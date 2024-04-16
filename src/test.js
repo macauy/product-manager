9d5e57af0ac643f3f81bc40581aeec01d544efa5
macauy <maca.rabosto@gmail.com>
2024-04-16 16:51:26 -0300

    cambios

--- src/test.js
+++ src/test.js
@@ -2,7 +2,7 @@ import ProductManager from "./ProductManager.js";
 
 const p1 = new ProductManager("./productos.json");
 
-let product1 = {
+const product1 = {
 	title: "titulo1",
 	description: "desc1",
 	price: 100,
@@ -11,7 +11,7 @@ let product1 = {
 	stock: 50,
 };
 
-let product2 = {
+const product2 = {
 	title: "titulo2",
 	description: "desc2",
 	price: 200,
@@ -20,7 +20,7 @@ let product2 = {
 	stock: 50,
 };
 
-let product3 = {
+const product3 = {
 	title: "titulo3",
 	description: "desc3",
 	price: 300,
@@ -29,7 +29,7 @@ let product3 = {
 	stock: 50,
 };
 
-let product4 = {
+const product4 = {
 	title: "titulo4",
 	description: "desc4",
 	price: 400,
@@ -37,7 +37,7 @@ let product4 = {
 	code: "code4",
 	stock: 500,
 };
-let product5 = {
+const product5 = {
 	title: "titulo5",
 	description: "desc5",
 	price: 400,
@@ -46,10 +46,59 @@ let product5 = {
 	stock: 500,
 };
 
+const product6 = {
+	title: "titulo6",
+	description: "desc6",
+	price: 100,
+	thumbnail: "thumb",
+	code: "code6",
+	stock: 50,
+};
+
+const product7 = {
+	title: "titulo7",
+	description: "desc7",
+	price: 200,
+	thumbnail: "thumb",
+	code: "code7",
+	stock: 50,
+};
+
+const product8 = {
+	title: "titulo8",
+	description: "desc8",
+	price: 300,
+	thumbnail: "thumb",
+	code: "code8",
+	stock: 50,
+};
+
+const product9 = {
+	title: "titulo9",
+	description: "desc9",
+	price: 400,
+	thumbnail: "thumb",
+	code: "code9",
+	stock: 500,
+};
+const product10 = {
+	title: "titulo10",
+	description: "desc10",
+	price: 400,
+	thumbnail: "thumb",
+	code: "code10",
+	stock: 500,
+};
+
 await p1.addProduct(product1);
 await p1.addProduct(product2);
 await p1.addProduct(product3);
 await p1.addProduct(product4);
 await p1.addProduct(product5);
+await p1.addProduct(product6);
+await p1.addProduct(product7);
+await p1.addProduct(product8);
+await p1.addProduct(product9);
+await p1.addProduct(product10);
 
 console.log(await p1.getProducts(0));
